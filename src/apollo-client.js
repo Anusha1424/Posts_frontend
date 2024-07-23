d3613c98023812c4a51c4923e73cb568f7badf79
Anusha1424 <38711643+Anusha1424@users.noreply.github.com>
2024-07-23 13:38:44 +1000

    Update apollo-client.js url

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 // Initialize Apollo Client
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/', // Your GraphQL server URI
+    uri: 'https://postapi-two.vercel.app/', // Your GraphQL server URI
     cache: new InMemoryCache(),
 });
 
